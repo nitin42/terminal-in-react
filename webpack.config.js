cd4ceb8e9eda52fbdacf0ba005041e8e1ca5293a
Nitin <tulswani19@gmail.com>
2017-07-08 11:11:41 +0530

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,32 @@
 const webpack = require('webpack');
-const { join, resolve } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const VENDOR = ['react', 'react-dom'];
 
 module.exports = {
-  entry: {
-    main: [
-      'webpack-dev-server/client?http://0.0.0.0:8080',
-      'webpack/hot/only-dev-server',
-      join(__dirname, 'starter/App.js')
-    ],
-    vendor: VENDOR
-  },
-  output: {
-    filename: '[name].[hash].js',
-    path: resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
-  devServer: {
-    publicPath: '/',
-    historyApiFallback: true,
-    hot: true,
-    port: '3000',
-    host: '127.0.0.1',
-    noInfo: true,
-    overlay: true,
-    clientLogLevel: "none"
-  },
-  stats: {
-    chunks: true,
-    chunkModules: true,
-    colors: true,
-    errors: true,
-    errorDetails: true,
-    timings: true,
-    version: true,
-    warnings: true
-  },
-  devtool: 'eval',
+  entry: './starter/App.js',
+  output: './starter/bundle.js',
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: [/node_modules/, /__tests__/, /coverage/],
+        exclude: [/node_modules/, /__tests__/, /docs/, /coverage/],
         use: ['babel-loader']
       },
       {
         test: /\.css$/,
-        exclude: /node_modules/,
+        include: /components/,
         use: ['style-loader', 'css-loader']
       }
     ]
   },
+  target: 'web',
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new HtmlWebpackPlugin({
-      template: require('html-webpack-template'),
-      appMountId: 'app'
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development')
+      }
     }),
     new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    })
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ]
 };
