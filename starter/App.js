fd23246c77b3eb9a91f72c3208c972895c526a18
Nitin <tulswani19@gmail.com>
2017-07-09 00:36:22 +0530

    init

--- starter/App.js
+++ starter/App.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 import Terminal from '../build/terminal.js';
 
 const App = () => (
-  <Terminal msg="Hello World. My name is Nitin Tulswani" />
+  <Terminal msg="Hello World. My name is Nitin Tulswani" watchConsoleLogging={true} />
 );
 
 render(<App />, document.getElementById('app'));
