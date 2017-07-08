6f4eba1521e3e5e07e32fb1bbc9356ce931d6bdf
Nitin <tulswani19@gmail.com>
2017-07-08 11:11:33 +0530

    entry

--- starter/App.js
+++ starter/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+// Bundle generated with npm run build:production 
 import Terminal from '../build/terminal.js';
 
 class App extends Component {
