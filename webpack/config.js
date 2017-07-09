067b773f9ebb940cad83199a95df4cfb0af3eea0
Nitin <tulswani19@gmail.com>
2017-07-10 00:02:31 +0530

    added compression plugin

--- webpack/config.js
+++ webpack/config.js
@@ -1,7 +1,8 @@
 const webpack = require('webpack'); // eslint-disable-line
 const { join, resolve } = require('path');
 const BabiliPlugin = require('babili-webpack-plugin'); // eslint-disable-line
-  
+const CompressionPlugin = require('compression-webpack-plugin');
+
 const common = {
   exclude: [
     /node_modules/,
@@ -13,8 +14,8 @@ const common = {
 
 const jsLoader = () => ({
   test: /\.js$/,
-  exclude: common.exclude,
-  use: ['babel-loader'],
+  include: [/components/, /node_modules\/args/], // Added module `args` because babel-loader skips everything from node_modules before transpiliing the code but we need args to be transpiled along with the components folder.
+  use: ['babel-loader']
 });
 
 const styleLoader = () => ({
@@ -48,6 +49,13 @@ const plugins = () => [
       NODE_ENV: JSON.stringify('production'),
     },
   }),
+  new CompressionPlugin({
+    asset: "[path].gz[query]",
+    algorithm: "gzip",
+    test: /\.js$|\.css$|\.html$/,
+    threshold: 10240,
+    minRatio: 0
+  })
 ];
 
 const externals = () => ({
